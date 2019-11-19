566a587d8aa4df983fa6f31a8903de49a487743a
jallelah-Al-Olayan <jallelahal@gmail.com>
2019-11-19 21:32:14 +0300

    new

---
+++ main.js
@@ -0,0 +1,13 @@
+//3.Create a main.js file and write the following functions
+
+//Write a function that console.logs each character object in the data.
+let logData = function() {
+}
+//Write a function that console.logs each character's name
+let logNames = function() {
+};
+//Write a function that console.logs each character's name and eye color
+let logNameEyes = function() {
+};
+//Write a function that console.logs each character's name whose mass is greater than 75
+let logByMass = function() {}
